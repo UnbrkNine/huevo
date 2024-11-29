d66a5da2f4a6f7bbbd09357f8eb8798bc3dd8259
UnbrkNine <dmpg550@gmail.com>
2024-11-29 13:37:45 -0600

    Update huevo.cpp

--- huevo.cpp
+++ huevo.cpp
@@ -2,12 +2,14 @@
 using namespade std;
 int main() {
   int n, num;
-  double sum
+  double sum;
+  
   do {
-  cout << "How many numbers are you going to type?" << endl;
-  cin >> n;
+    cout << "How many numbers are you going to type?" << endl;
+    cin >> n;
   } while (n < 1 || n > 10);
-    for(intc = 1; c<n; c++){
+  
+  for(intc = 1; c<n; c++){
     cout<<c;
   }
   return 0;
