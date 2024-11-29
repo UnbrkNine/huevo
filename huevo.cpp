2f51ac64be2b7fcb5065ea8f45f5e4dfb6a792e9
Andrea <a01286027@tec.mx>
2024-11-29 13:35:27 -0600

    Update huevo.cpp
    Add a validation loop.

--- huevo.cpp
+++ huevo.cpp
@@ -3,13 +3,13 @@ using namespade std;
 int main() {
   int n, num;
   double sum
+  do {
   cout << "How many numbers are you going to type?" << endl;
   cin >> n;
-
-  for(intc = 1; c<n; c++){
+  } while (n < 1 || n > 10);
+    for(intc = 1; c<n; c++){
     cout<<c;
   }
-  
   return 0;
 }
 
